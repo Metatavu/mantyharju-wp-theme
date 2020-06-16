2507e9ad07e6c7ab119537244d1c3cf3d5cf4ae2
Eetu Lepisto <aeele001@edu.xamk.fi>
2020-06-16 10:41:17 +0300

    Removed loader template and Muisti-logo, fixed typo

--- inc/template-head-generic.php
+++ inc/template-head-generic.php
@@ -3,9 +3,7 @@
 <meta property="og:locale" content="<?php echo get_locale();?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdn.metatavu.io/fonts/tt_norms/stylesheet.css" />
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/inc/loader.css">
-<link rel="icon" type="image/png" href="<?php bloginfo('template_url') ?>/inc/favicon.png" sizes="512x512" />
-<title>Muisti</title>
+<title>Mäntyharju</title>
 <script>
   window.THEME_PATH = "<?php bloginfo('template_url');?>";
 </script>
