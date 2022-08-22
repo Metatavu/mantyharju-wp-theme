eaedfcbc0b45e82fa6c0f6ae235f8bf6c8d98b0e
Tuomas Kämppi <tuomas.kamppi@metatavu.fi>
2022-08-22 10:35:35 +0300

    metadata no longer fetched from latest post

--- inc/template-head-generic.php
+++ inc/template-head-generic.php
@@ -1,9 +1,9 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="Content-language" content="<?php echo get_locale();?>">
 <meta property="og:locale" content="<?php echo get_locale();?>" />
-<meta property="og:url" content="<?php echo get_permalink();?>" />
-<meta property="og:title" content="<?php echo get_the_title();?>" />
-<meta property="og:description" content='<?php $excerpt = strip_tags(get_the_excerpt()); echo $excerpt; ?>'/>
+<meta property="og:url" content="https://www.mantyharju.fi/" />
+<meta property="og:title" content="Mäntyharju" />
+<meta property="og:description" content="Luontoa, kulttuuria, elämää!"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,600&family=Rubik&display=swap" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/inc/styles.css" />
