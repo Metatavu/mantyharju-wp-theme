f7e0dbfd2541f096b487baa141503c5b2a752284
Antti Leppä <antti.leppa@metatavu.fi>
2023-02-24 11:24:55 +0200

    Fixed issue #339

--- sptv/components/service_location_service_channel/service-hours.php
+++ sptv/components/service_location_service_channel/service-hours.php
@@ -3,6 +3,6 @@
   include_once $paths["common"];
 
   $serviceChannel = $data->serviceChannel;
-  echo formatServiceHours($serviceChannel["serviceHours"]);
+  echo formatServiceHours($serviceChannel["serviceHours"], $data->language);
 
 ?>
